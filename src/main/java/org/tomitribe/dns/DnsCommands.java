a5c51aac28302dfdabdba8d1d6da53798e8e133a
Jonathan Gallimore <jon@jrg.me.uk>
2019-09-30 22:56:05 +0100

    Add count parameter

--- src/main/java/org/tomitribe/dns/DnsCommands.java
+++ src/main/java/org/tomitribe/dns/DnsCommands.java
@@ -17,6 +17,7 @@
 package org.tomitribe.dns;
 
 import org.tomitribe.crest.api.Command;
+import org.tomitribe.crest.api.Default;
 import org.tomitribe.crest.api.Option;
 import org.tomitribe.crest.api.Required;
 import org.tomitribe.crest.api.StreamingOutput;
@@ -37,7 +38,7 @@ import java.util.TreeSet;
 public class DnsCommands {
 
     @Command("check-local")
-    public StreamingOutput checkLocal() {
+    public StreamingOutput checkLocal(final @Option("count") @Default("5") Integer iterations) {
         return new StreamingOutput() {
             @Override
             public void write(final OutputStream outputStream) throws IOException {
@@ -46,18 +47,54 @@ public class DnsCommands {
                 writer.println("Looking up localhost...\n");
                 writer.flush();
 
-                final long start = System.currentTimeMillis();
-                final InetAddress localHost = InetAddress.getLocalHost();
-                final long end = System.currentTimeMillis();
+                for (int i = 0; i < iterations; i++) {
+                    final long start = System.currentTimeMillis();
+                    final InetAddress localHost = InetAddress.getLocalHost();
+                    final long end = System.currentTimeMillis();
 
-                writer.println(String.format("Found in %d ms: %s", (end - start), localHost.toString()));
+                    writer.println(String.format("Found in %d ms: %s", (end - start), localHost.toString()));
+                    writer.flush();
+                }
+            }
+        };
+    }
+
+    @Command("check-reverse")
+    public StreamingOutput checkReverse(final @Required @Option("address") String address, final @Option("count") @Default("5") Integer iterations) {
+        return new StreamingOutput() {
+            @Override
+            public void write(final OutputStream outputStream) throws IOException {
+                final PrintWriter writer = new PrintWriter(outputStream);
+
+                writer.println(String.format("Looking up %s...\n", address));
                 writer.flush();
+
+                final String[] parts = address.split("\\.");
+
+                if (parts.length != 4) {
+                    throw new IllegalArgumentException("Address provided was not a valid IPv4 address");
+                }
+
+                byte[] ipAddress = new byte[4];
+
+                for (int i = 0; i < 4; i++) {
+                    ipAddress[i] = Byte.parseByte(parts[i]);
+                }
+
+                for (int i = 0; i < iterations; i++) {
+                    final long start = System.currentTimeMillis();
+                    final InetAddress inetAddress = InetAddress.getByAddress(ipAddress);
+                    final long end = System.currentTimeMillis();
+
+                    writer.println(String.format("Found in %d ms: %s", (end - start), inetAddress.toString()));
+                    writer.flush();
+                }
             }
         };
     }
 
     @Command("check-java")
-    public StreamingOutput checkJava(final @Required @Option("hostname") String hostName) {
+    public StreamingOutput checkJava(final @Required @Option("hostname") String hostName, final @Option("count") @Default("5") Integer iterations) {
         return new StreamingOutput() {
             @Override
             public void write(final OutputStream outputStream) throws IOException {
@@ -66,19 +103,23 @@ public class DnsCommands {
                 writer.println(String.format("Looking up %s...\n", hostName));
                 writer.flush();
 
-                final long start = System.currentTimeMillis();
-                final InetAddress address = InetAddress.getByName(hostName);
-                final long end = System.currentTimeMillis();
+                for (int i = 0; i < iterations; i++) {
+                    final long start = System.currentTimeMillis();
+                    final InetAddress address = InetAddress.getByName(hostName);
+                    final long end = System.currentTimeMillis();
 
-                writer.println(String.format("Found in %d ms: %s", (end - start), address.toString()));
-                writer.flush();
+                    writer.println(String.format("Found in %d ms: %s", (end - start), address.toString()));
+                    writer.flush();
+                }
             }
         };
     }
 
 
     @Command("check-jndi")
-    public StreamingOutput check(final @Required @Option("hostname") String hostName, final @Required @Option("type") String type) {
+    public StreamingOutput check(final @Required @Option("hostname") String hostName, final @Required @Option("type") String type,
+                                 final @Option("count") @Default("5") Integer iterations) {
+
         return new StreamingOutput() {
             @Override
             public void write(OutputStream outputStream) throws IOException {
@@ -87,38 +128,40 @@ public class DnsCommands {
                 writer.println(String.format("Looking up %s record for %s...\n", type, hostName));
                 writer.flush();
 
-                final long start = System.currentTimeMillis();
-                final Set<String> results = new TreeSet<>();
-                try {
-                    final Hashtable<String, String> envProps = new Hashtable<>();
-                    envProps.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.dns.DnsContextFactory");
+                for (int i = 0; i < iterations; i++) {
+                    final long start = System.currentTimeMillis();
+                    final Set<String> results = new TreeSet<>();
+                    try {
+                        final Hashtable<String, String> envProps = new Hashtable<>();
+                        envProps.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.dns.DnsContextFactory");
 
-                    final DirContext dnsContext = new InitialDirContext(envProps);
-                    final Attributes dnsEntries = dnsContext.getAttributes(hostName, new String[]{type});
+                        final DirContext dnsContext = new InitialDirContext(envProps);
+                        final Attributes dnsEntries = dnsContext.getAttributes(hostName, new String[]{type});
 
-                    if (dnsEntries != null) {
-                        final NamingEnumeration<?> dnsEntryIterator = dnsEntries.get(type).getAll();
-                        while(dnsEntryIterator.hasMoreElements()) {
-                            results.add(dnsEntryIterator.next().toString());
+                        if (dnsEntries != null) {
+                            final NamingEnumeration<?> dnsEntryIterator = dnsEntries.get(type).getAll();
+                            while (dnsEntryIterator.hasMoreElements()) {
+                                results.add(dnsEntryIterator.next().toString());
+                            }
                         }
+                    } catch (Exception e) {
+                        writer.println("Error while looking up record: " + e.getMessage());
+                        e.printStackTrace(writer);
+                        writer.flush();
                     }
-                } catch (Exception e) {
-                    writer.println("Error while looking up record: " + e.getMessage());
-                    e.printStackTrace(writer);
+
+                    final long end = System.currentTimeMillis();
+                    writer.println(String.format("Lookup completed in %d ms", (end - start)));
+                    writer.flush();
+
+                    writer.println(String.format("Found %d records", results.size()));
+
+                    for (final String result : results) {
+                        writer.println(String.format("Result: %s", result));
+                    }
+
                     writer.flush();
                 }
-
-                final long end = System.currentTimeMillis();
-                writer.println(String.format("Lookup completed in %d ms", (end - start)));
-                writer.flush();
-
-                writer.println(String.format("Found %d records", results.size()));
-
-                for (final String result : results) {
-                    writer.println(String.format("Result: %s", result));
-                }
-
-                writer.flush();
             }
         };
     }
